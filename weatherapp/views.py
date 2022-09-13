3b1500eae297b3eee9c44fe4461a5879eda9231a
Askin <asknkskk@gmail.com>
2022-09-13 21:41:10 +0300

    add city models

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -2,17 +2,32 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
+
+from weatherapp.models import City
 
 def index(request):
     API_KEY = config("API_KEY")
-    city = "Yozgat"
+    city = "Erzurum"
+    u_city = request.POST.get("name")
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        # print(response.ok)   # yazılan city varsa true yoksa false geliyor...
+        if response.ok:
+            content = response.json()
+            r_city = content["name"]
+            if City.objects.filter(name=r_city):
+                messages.warning(request, "City already exists!")
+            else:
+                City.objects.create(name=   r_city)
+        else:
+            messages.warning(request, "There is no city")
+
+
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
-    content = response.json()
-    pprint(content["name"])
-    pprint(content["main"]["temp"])
-    pprint(content["weather"][0]["description"])
-    pprint(content["weather"][0]["icon"])
+    content = response.json()    
     
     context = {
         "city" : content["name"],
